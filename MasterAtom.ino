87a9f5e380a3401f1a17fe1896e22dec9ce594d1
colinpeth <43682220+colinpeth@users.noreply.github.com>
2019-04-18 17:32:27 -0400

    Correct Code For Master

--- MasterAtom.ino
+++ MasterAtom.ino
@@ -1,108 +1,96 @@
-#include <Wire.h>
 #include <WiFi.h>
-#include <Adafruit_BMP085.h>
-Adafruit_BMP085 bmp;
-#include <dht.h>
-#define dataPin 15 // Defines pin number to which the sensor is connected
-dht DHT; // Creats a DHT object
-int rainPin = 2;
-int thresholdValueOfRain = 1400;
-char ssid[] = "WeatherStation";           // SSID of your AP
-char pass[] = "ilikeweather";         // password of your AP
-IPAddress server(192,168,4,15);     // IP address of the AP
-WiFiClient client;
-void setup()
-{
+
+WiFiServer server(80);                // Set a object server as a WiFiServer class
+IPAddress IP(192,168,4,15);           // Select ip and mask
+IPAddress mask = (255, 255, 255, 0);
+
+// The devices that want to connect to our Access Point will need to use the next credentials.
+char ssid[] = "WeatherStation";           // SSID for the Access Point
+char pass[] = "ilikeweather";         // password for the Access Point
+
+byte EspLed = 2;                      // Built in lED on the ESP32
+
+void setup() {
+
   Serial.begin(115200);
-  if (!bmp.begin())
-  {
-  Serial.println("BMP180 sensor not found");
-  while (1) {}
-  }
-   WiFi.mode(WIFI_STA);              // Wifi as client mode
-  WiFi.begin(ssid, pass);           // Credentials to connect to the Access Point
 
-  Serial.println("Connection to the AP");
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(500);
+  WiFi.mode(WIFI_AP);                     // Set Wi-Fi as access point/server
+  WiFi.softAP(ssid, pass);                // SSID and Password for the AP
+  WiFi.softAPConfig(IP, IP, mask);        // Set our own desired IP address
+  server.begin();                         // Begin the server
 
-  }
-  Serial.println("Connected");
-  Serial.print("LocalIP:");
-  Serial.println(WiFi.localIP());
-  Serial.println("MAC:" + WiFi.macAddress());
-  Serial.print("Gateway:");
-  Serial.println(WiFi.gatewayIP());
-  Serial.print("AP MAC:");
-  Serial.println(WiFi.BSSIDstr());
+  pinMode(EspLed, OUTPUT);                // Set Built-in LED as output
 
+  Serial.println("Server started.");
+  Serial.print("IP: ");
+  Serial.println(WiFi.softAPIP());        // .softAPIP calls for the IP of the access point which we set earlier
+  Serial.print("MAC:");
+  Serial.println(WiFi.softAPmacAddress());  // Calls for the mac address
 }
 
 void loop() {
 
+  WiFiClient client = server.available(); // Return a client object to the class if there is a client available
+  if (!client) {return;}                  // Return cuts the function (loop) if client class is not connected
 
+  digitalWrite(EspLed, LOW);
 
-    //DHT11
-    int readData = DHT.read22(dataPin); // Reads the data from the sensor
-    float DHTtempC = DHT.temperature; // Gets the values of the temperature
-    float DHThum = DHT.humidity; // Gets the values of the humidity
+  String IBMPTempC = client.readStringUntil('\r');
+  String IBMPPres = client.readStringUntil('\r');
+  String IDHTTempC = client.readStringUntil('\r');
+  String IDHTHum = client.readStringUntil('\r');
+  String IRainVal = client.readStringUntil('\r');
 
-    //BMP180 Temp and Pressure
-    double BMPtempC=bmp.readTemperature();//Gets Temp from BMP
-    double BMPPres=bmp.readPressure(); //Gets Altitude from BMP
+  double BMPTempC=IBMPTempC.toDouble();
+  double BMPPresPa=IBMPPres.toDouble();
+  double DHTTempC = IDHTTempC.toDouble();
+  double DHTHum = IDHTHum.toDouble();
+  double RainVal = IRainVal.toDouble();
 
-    //Temp Average and F conversion
-    double TempC=(DHTtempC+BMPtempC)/2;
-    double TempF=TempC*9/5+32;
+  //C to F conversions
+  double TempC=(DHTTempC+BMPTempC)/2;
+  double TempF=TempC*9/5+32;
 
-    //Rain Sensor
-    int RainVal = analogRead(rainPin);
+  //Pressure conversion
+  double PresATM = BMPPresPa/101325;
 
-
+  //Print
     Serial.print("Temperature BMP = ");
-    Serial.println(BMPtempC);
+    Serial.println(BMPTempC);
     Serial.print("Temperature DHT = ");
-    Serial.println(DHTtempC);
+    Serial.println(DHTTempC);
 
-    Serial.println("Average of Sensors Temperature = ");
+    Serial.print("Average of Sensors Temperature = ");
     Serial.print(TempC);
-    Serial.print(" *C");
-    Serial.println("Temperature = ");
+    Serial.println(" *C");
+    Serial.print("Temperature = ");
     Serial.print(TempF);
     Serial.println(" *F");
 
-    Serial.println("Humidity = ");
-    Serial.print(DHThum);
-    Serial.print("%");
+    Serial.print("Humidity = ");
+    Serial.print(DHTHum);
+    Serial.println("%");
 
-    Serial.println("Pressure = ");
-    Serial.print(BMPPres);
+    Serial.print("Pressure = ");
+    Serial.print(BMPPresPa);
     Serial.println(" Pa");
 
+    Serial.print("Pressure = ");
+    Serial.print(PresATM);
+    Serial.println(" atm");
 
 
+    //Rain If Else
+    Serial.print(RainVal);
+    Serial.print("Rain: ");
+     if(RainVal < 1600){
+    Serial.println(" Yes ");
 
-     if (!client.connect(server, 80)) {          // Connect to the server through the port 80
-          Serial.println("connection failed");  // If server not connected run connection again
-          return;                                 // return terminates the function (loop)
-      }
+    }
+    else {
+    Serial.println(" No ");
+     }
 
-    client.print(BMPtempC);
-    client.print('\r');
-    client.print(BMPPres);
-    client.print('\r');
-    client.print(DHTtempC);
-    client.print('\r');
-    client.print(DHThum);
-    client.print('\r');
-    client.print(RainVal);
-    client.print('\r');
-
-
-  String answer = client.readStringUntil('\r');   // Receive value from the server until \r
-  Serial.println(answer);
- // client.flush();
-  client.stop();
-
+    Serial.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+  digitalWrite(EspLed, HIGH);
 }
